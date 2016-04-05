42324b5d509c6c08535b1a3bd93c8ed6926c828b
Craig Waterman <craigwaterman@gmail.com>
2016-04-05 15:22:23 -0400

    Minor script optimizations for python

--- benchbs4.py
+++ benchbs4.py
@@ -1,24 +1,11 @@
 import glob
 import codecs
-import json
-import os
-import http.client
-import socket
-import urllib.parse
-import urllib.request
-import urllib.error
-import re
-import sys
-import hashlib
-import snappy
-import psutil
-import multiprocessing
 from bs4 import BeautifulSoup, Comment
 
 for item in glob.glob("./test_files/*.html"):
     f = open(item, 'r', encoding='utf8')
     html_content = f.read()
-    clean_html = html_content #re.sub('</?o:(p|style|div|st\d|!\[)[^>]*?>', '', html_content)
+    clean_html = html_content 
     soup = BeautifulSoup(clean_html, "lxml")
     links = soup.find_all('a')
     anchors = ""
@@ -29,4 +16,3 @@ for item in glob.glob("./test_files/*.html"):
     output = codecs.open(new_path, mode='wb')
     output.write(bytes(anchors, 'UTF-8'))
     output.close
-
